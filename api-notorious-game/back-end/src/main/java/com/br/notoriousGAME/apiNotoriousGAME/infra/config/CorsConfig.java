58ff9d442c5138b0381493400781de1924fcaf04
lzphelipe <luizphelipe99875@gmail.com>
2025-11-29 22:02:54 -0300

    Prepara o Front para o Security

--- api-notorious-game/back-end/src/main/java/com/br/notoriousGAME/apiNotoriousGAME/infra/config/CorsConfig.java
+++ api-notorious-game/back-end/src/main/java/com/br/notoriousGAME/apiNotoriousGAME/infra/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.br.notoriousGAME.apiNotoriousGAME.config;
+package com.br.notoriousGAME.apiNotoriousGAME.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
