728b59c16bc604b30f4a0e0497a5eb2f6d97ac49
Gui Maia <guilhermemaia05@outlook.com>
2025-12-01 15:40:42 -0300

    fix(cadastro_categoria): corrige conflito de código

--- api-notorious-game/front-end/src/pages/cadastro_categoria/index.jsx
+++ api-notorious-game/front-end/src/pages/cadastro_categoria/index.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import './style.css'
+import { useNavigate } from 'react-router-dom' 
+import styles from './style.module.css' 
+import api from '../../services/api' 
+
 import LogoImg from '../../assets/logo_notorious.png'
 import Perfil from '../../assets/do-utilizador.png'
-import Carrinho from '../../assets/carrinho-de-compras.png'
 
 function CadastrarCategoria() {
+  const navigate = useNavigate()
   
-  // Estado para guardar os dados do formulário
   const [form, setForm] = useState({
-    nome: '',
+    nomeCategoria: '',
     descricao: ''
   })
 
@@ -16,51 +18,92 @@ function CadastrarCategoria() {
     setForm({ ...form, [e.target.name]: e.target.value })
   }
 
-  return (
-    <div className="layout-admin">
+  async function salvarCategoria() {
+    // Validação simples
+    if (!form.nomeCategoria || !form.descricao) {
+      alert("Preencha todos os campos!")
+      return
+    }
+
+    const token = localStorage.getItem('token')
+
+    try {
+      // Envia para o Back-end
+      await api.post('/categorias', form, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+
+      alert("Categoria cadastrada com sucesso!")
       
-      {/* HEADER (Igual ao das outras páginas) */}
-      <header className="top-bar">
-        <button className="logo-area"> 
-            <img src={LogoImg} alt="Logo Notorious" className="logo-img" /> 
-        </button>
-        <div className="top-icons">
-          <button className='btn-icone'> <img src={Carrinho} className="icone-img" /> </button>
-          <button className='btn-icone'> <img src={Perfil} className="icone-img" /> </button>
+      // Redireciona de volta para a tela de Gerenciar Jogos
+      navigate('/jogos') 
+
+    } catch (error) {
+      console.error("Erro ao cadastrar categoria", error)
+      alert("Erro ao cadastrar. Verifique o console.")
+    }
+  }
+
+  return (
+    <div className={styles['layout-admin']}>
+      
+      {/* HEADER */}
+      <header className={styles['top-bar']}>
+        <div className={styles['logo-area']} onClick={() => navigate('/home')}> 
+            <img src={LogoImg} alt="Logo Notorious" className={styles['logo-img']} /> 
+        </div>
+        <div className={styles['top-icons']}>
+          <button className={styles['btn-icone']} onClick={() => navigate('/')}> <img src={Perfil} className={styles['icone-img']} /> </button>
         </div>
       </header>
 
-      <main className="main-content">
+      <main className={styles['main-content']}>
         
-        {/* O CONTAINER PRETO */}
-        <div className="black-container">
+        {/* O CONTAINER PRETO DIVIDIDO */}
+        <div className={`${styles['black-container']} ${styles['cadastro-layout']}`}>
            
            {/* LADO ESQUERDO: O Formulário */}
-           <div className="form-esquerda">
+           <div className={styles['form-esquerda']}>
+              <label className={styles['label-form']}>Nome da Categoria</label>
               <input 
-                placeholder="Nome do Jogo" 
-                name="nome" 
-                value={form.nome} 
+                className={styles['input-nome']} 
+                placeholder="Nome" 
+                name="nomeCategoria" 
+                value={form.nomeCategoria} 
                 onChange={handleChange} 
               />
+
+              <label className={styles['label-form']}>Descrição</label>
               <input 
+                className={styles['input-padrao']} 
                 placeholder="Descrição" 
                 name="descricao" 
-                value={form.desenvolvedora} 
+                value={form.descricao} 
                 onChange={handleChange} 
               />
            </div>
 
            {/* O RISCO NO MEIO */}
-           <div className="divisor-vertical"></div>
+           <div className={styles['divisor-vertical']}></div>
 
            {/* LADO DIREITO: Texto e Botões */}
-           <div className="form-direita">
+           <div className={styles['form-direita']}>
               <h2>Você deseja salvar essa categoria no banco de dados?</h2>
               
-              <div className="grupo-botoes">
-                 <button className="btn-cancelar">Cancelar</button>
-                 <button className="btn-salvar">Salvar</button>
+              <div className={styles['grupo-botoes']}>
+                 <button 
+                    className={styles['btn-cancelar']}
+                    onClick={() => navigate('/jogos')} // Cancela e volta
+                 >
+                    Cancelar
+                 </button>
+                 
+                 <button 
+                    className={styles['btn-salvar']}
+                    onClick={salvarCategoria}
+                 >
+                    Salvar
+                 </button>
               </div>
            </div>
 
